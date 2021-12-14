13f7dd393fcc5e282699ddf1bd775191bb877cc2
Gerolf <gerolf.scherr@cenarion.com>
2021-12-14 21:09:43 +0100

    day 14

--- src/AOC14.java
+++ src/AOC14.java
@@ -3,55 +3,49 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 public class AOC14 {
-
-
 
     public static void main(String[] args) throws IOException {
         new AOC14().start(args[0]);
     }
 
-
-
     private void start(String fn) throws IOException {
         Path f = Paths.get(fn);
         String in = Files.lines(f).findFirst().get();
         var map =  Files.lines(f).skip(2).map(s -> s.split(" -> ")).collect(Collectors.toMap(s->s[0], s->s[1]));
-        process1(in, map);
-        process2(in, map);
+        //process1(in, map);
+        process2(in, map, 10);
+        process2(in, map, 40);
     }
 
-
-
-    void process2(String in, Map<String, String>dict) {
+    void process2(String in, Map<String, String>dict, int rounds) {
         var pairs = new HashMap<String, Long>();
         for (int i = 0 ; i < in.length()-1; i++) pairs.merge(in.substring(i, i+2), 1l, Long::sum);
         var letters = in.chars().boxed().map(s->Character.toString(s)).collect(Collectors.toMap(x->x, x->1l, Long::sum));
 
-        for (int i = 0 ; i < 40; i++) {
+        for (int i = 0 ; i < rounds; i++) {
             var np = new HashMap<String, Long>();
             for (var kv : pairs.entrySet()) {
                 String pair = kv.getKey();
-                Long v = kv.getValue();
+                Long c = kv.getValue();
                 String m = dict.get(pair);
         //        if (m == null) throw new IllegalArgumentException(pair);
                 String l = pair.substring(0, 1),  r = pair.substring(1, 2);
 
-                np.merge(l+m, v, Long::sum);
-                np.merge(m+r, v, Long::sum);
-                letters.merge(m, v, Long::sum);
+                np.merge(l+m, c, Long::sum);
+                np.merge(m+r, c, Long::sum);
+                letters.merge(m, c, Long::sum);
             }
             pairs = np;
         }
         var freq= letters.values().stream().sorted().toList();
-        System.out.println("diff:" + (freq.get(freq.size()-1) - freq.get(0)));
+        System.out.println("part:" + (freq.get(freq.size()-1) - freq.get(0)));
     }
 
 
-
+/*
     private void process1(String in, Map<String, String> map) {
         for (int j = 0; j < 10; j++) {
             var b = new StringBuilder();
@@ -66,8 +60,8 @@ public class AOC14 {
 
         var letters = in.chars().boxed().collect(Collectors.groupingBy(s-> Character.toString(s), Collectors.counting()));
         var c = letters.values().stream().sorted().toList();
-        System.out.println("z:" + (c.get(c.size()-1) -  c.get(0)));
+        System.out.println("part1:" + (c.get(c.size()-1) -  c.get(0)));
     }
-
+*/
 
 }
